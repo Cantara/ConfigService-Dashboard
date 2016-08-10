56a2b5edbabb9312e2319e688d1d43f9ace5a0f0
totto <totto@totto.org>
2016-08-10 09:26:10 +0200

    new attempt

--- src/main/java/no/cantara/csdb/commands/CommandGetAllClients.java
+++ src/main/java/no/cantara/csdb/commands/CommandGetAllClients.java
@@ -4,6 +4,10 @@ package no.cantara.csdb.commands;
 public class CommandGetAllClients extends BaseGetCommand<String>{
 
 
+    @Override
+    protected String getCacheKey() {
+        return String.valueOf("");
+    }
 
 	@Override
 	protected String getTargetPath() {
