f4c3fa4540a3debfa37a034c4c2548cf4fd52704
totto <totto@totto.org>
2016-08-10 09:22:02 +0200

    added testing of using cache in hystrix commands

--- src/main/java/no/cantara/csdb/commands/CommandGetAllClients.java
+++ src/main/java/no/cantara/csdb/commands/CommandGetAllClients.java
@@ -4,10 +4,6 @@ package no.cantara.csdb.commands;
 public class CommandGetAllClients extends BaseGetCommand<String>{
 
 
-	@Override
-	protected String getCacheKey() {
-		return String.valueOf(null);
-	}
 
 	@Override
 	protected String getTargetPath() {
