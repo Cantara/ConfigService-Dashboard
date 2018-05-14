e37ec1f2bdb214a0c0e3c6ed12689fdfe3f9686b
Anders Rønning <andeero@ifi.uio.no>
2018-05-14 17:39:29 +0200

    Add script for optionally pulling properties from AWS Parameter Store

--- DockerAWS/toRoot/runapp.sh
+++ DockerAWS/toRoot/runapp.sh
@@ -6,42 +6,15 @@ JAVA_PARAMS="-Dlogback.configurationFile=./config_override/logback.xml -Xms64m -
 date +" --- RUNNING $(basename $0) %Y-%m-%d_%H:%M:%S --- "
 set -x
 
-if [ "$AWS_CLOUDWATCH_LOGGING_ENABLED" = "true" ]; then
-    # Enables aws cloudwatch plugin in aws cli
-    aws configure set plugins.cwlogs cwlogs
-    # Sets container id used by as part of log-stream-name
-    CONTAINER_ID="$(cat /proc/self/cgroup | grep docker | grep -o -E '[0-9a-f]{64}' | head -n 1 | cut -c1-12)"
-    #replaces log-group and log-stream name in aws-cloudwatch.conf
-    sed -i -e "s/{CONTAINER_ID}/$CONTAINER_ID/g" aws-cloudwatch.conf
-    sed -i -e "s/{AWS_LOG_GROUP}/$AWS_LOG_GROUP/g" aws-cloudwatch.conf
-    if [ -z "$LOGBACK_CANTARA_LEVEL" ]; then
-        sed -i -e "s/{LOGBACK_CANTARA_LEVEL}/info/g" config_override/logback.xml
+if [ "$AWS_PARAMETER_STORE_ENABLED" = "true" ]; then
+    python GetPropertiesFromParameterStore.py $AWS_PARAMETER_STORE_PATH config_override/application_override.properties
+    if [ $? -eq 0 ]; then
+        echo 'GetPropertiesFromParameterStore.py exited successfully!'
     else
-        sed -i -e "s/{LOGBACK_CANTARA_LEVEL}/$LOGBACK_CANTARA_LEVEL/g" config_override/logback.xml
+        echo 'GetPropertiesFromParameterStore.py exited with error (non null exit code)'
+        exit 1
     fi
-
-
-    java_pid=()
-    awslogs_pid=()
-    gotsigchld=false
-    #Using trap on CHLD and EXIT to run clean exit of java-process.
-    trap '
-    if ! "$gotsigchld"; then
-        gotsigchld=true
-        kill "${java_pid[@]}"
-        #When java process dies, give awslogs time to send last batch.
-        wait "${java_pid[@]}"
-        sleep 10
-        kill "${awslogs_pid[@]}"
-    fi
-    ' CHLD EXIT
-
-    /usr/bin/java $JAVA_PARAMS $JAVA_PARAMS_OVERRIDE -jar $APP & java_pid+=$!
-    aws logs push --region=$AWS_CLOUDWATCH_REGION --config-file aws-cloudwatch.conf & awslogs_pid+=$!
-    set -m
-    wait
-    set +m
-else
-    #If not running with cloudwatch enabled, just execute jar.
-    java $JAVA_PARAMS $JAVA_PARAMS_OVERRIDE -jar $APP
 fi
+
+set +x
+java $JAVA_PARAMS $JAVA_PARAMS_OVERRIDE -jar $APP
